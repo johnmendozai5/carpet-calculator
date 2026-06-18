545800dfd2653a1d346e24f10b0a351dacf9ff3d
johnmendozai5 <jmendoz223@gmail.com>
2026-06-17 21:07:27 -0500

    Step 2: add labor cost at /bin/zsh.75 per actual sq ft

--- main.cpp
+++ main.cpp
@@ -16,12 +16,14 @@ int main(){
 
     double carpet_cost = price_per_square_foot * room_width * room_length;
     double production_price = (carpet_cost * .2) + carpet_cost;
+    double labor_cost = (room_width * room_length) * 0.75;
     std::cout << std::fixed << std::setprecision(2);
 
 
 
     std::cout << "Room: " << (room_width * room_length) << " sq ft" << std::endl;
     std::cout << "Carpet: $" << production_price << std::endl;
+    std::cout << "Labor: $" << labor_cost << std::endl;
 
 
 
