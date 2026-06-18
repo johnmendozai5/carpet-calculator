98c2f33d392bf7a3485e18119c9c7b9a3c57bd0b
johnmendozai5 <jmendoz223@gmail.com>
2026-06-17 21:31:49 -0500

    Step 3: add 7% sales tax and total cost

--- main.cpp
+++ main.cpp
@@ -17,6 +17,8 @@ int main(){
     double carpet_cost = price_per_square_foot * room_width * room_length;
     double production_price = (carpet_cost * .2) + carpet_cost;
     double labor_cost = (room_width * room_length) * 0.75;
+    double gov_tax = (production_price + labor_cost) * 0.07;
+    double final_cost = production_price + labor_cost + gov_tax;
     std::cout << std::fixed << std::setprecision(2);
 
 
@@ -24,6 +26,8 @@ int main(){
     std::cout << "Room: " << (room_width * room_length) << " sq ft" << std::endl;
     std::cout << "Carpet: $" << production_price << std::endl;
     std::cout << "Labor: $" << labor_cost << std::endl;
+    std::cout << "Tax: $" << gov_tax << std::endl;
+    std::cout << "Cost: $" << final_cost << std::endl;
 
 
 
