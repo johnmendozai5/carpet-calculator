353c14a8040358d2e6baa3cf55e7665ebbf64381
john m <johnm@Johnnys-MacBook-Pro.local>
2026-06-18 19:31:54 -0500

    Step 4: loop through orders with running total

--- main.cpp
+++ main.cpp
@@ -7,34 +7,32 @@ int main(){
     double price_per_square_foot;
     int room_width;
     int room_length;
-    
 
-
-    std::cin >> price_per_square_foot;
-    std::cin >> room_width;
-    std::cin >> room_length;
-
-    double carpet_cost = price_per_square_foot * room_width * room_length;
-    double production_price = (carpet_cost * .2) + carpet_cost;
-    double labor_cost = (room_width * room_length) * 0.75;
-    double gov_tax = (production_price + labor_cost) * 0.07;
-    double final_cost = production_price + labor_cost + gov_tax;
+    double total_sales = 0;
     std::cout << std::fixed << std::setprecision(2);
 
-
-
-    std::cout << "Room: " << (room_width * room_length) << " sq ft" << std::endl;
-    std::cout << "Carpet: $" << production_price << std::endl;
-    std::cout << "Labor: $" << labor_cost << std::endl;
-    std::cout << "Tax: $" << gov_tax << std::endl;
-    std::cout << "Cost: $" << final_cost << std::endl;
-
     for(int i = 0; i < 2; i++){
-        
+        std::cout << "Order #" << (i + 1) << std::endl;
+
+        std::cin >> price_per_square_foot;    
+        std::cin >> room_width;
+        std::cin >> room_length;
+
+        double carpet_cost = price_per_square_foot * room_width * room_length;
+        double production_price = (carpet_cost * .2) + carpet_cost;
+        double labor_cost = (room_width * room_length) * 0.75;
+        double gov_tax = (production_price + labor_cost) * 0.07;
+        double final_cost = production_price + labor_cost + gov_tax;
+        total_sales += final_cost;
+
+        std::cout << "Room: " << (room_width * room_length) << " sq ft" << std::endl;
+        std::cout << "Carpet: $" << production_price << std::endl;
+        std::cout << "Labor: $" << labor_cost << std::endl;
+        std::cout << "Tax: $" << gov_tax << std::endl;
+        std::cout << "Cost: $" << final_cost << std::endl;
+        std::cout << std::endl;
     }
 
-
-
-
+    std::cout << "Total Sales: $" << total_sales << std::endl;
 }
     
